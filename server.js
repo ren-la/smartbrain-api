4f057b8d54ba418cacb6cb7cc1d4203e0e369197
renla <eslbauer.verena@gmail.com>
2022-04-17 17:53:34 +0200

    Connect to frontend

--- server.js
+++ server.js
@@ -1,7 +1,88 @@
 import express from 'express';
+import cors from 'cors';
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000')
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json(database.users);
+});
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email &&
+      req.body.password === database.users[0].password) {
+        res.json(database.users[0]);
+      } else {
+        res.status(400).json('Error logging in');
+      }
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length-1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  for (const user of database.users) {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  }
+  if (!found) {
+    res.status(400).json('User not found');
+  }
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  for (const user of database.users) {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  }
+  if (!found) {
+    res.status(400).json('User not found');
+  }
+})
+
+app.listen(3001, () => {
+  console.log('App is running on port 3001')
 })
